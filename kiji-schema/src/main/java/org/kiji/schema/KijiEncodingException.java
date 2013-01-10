c1c76c1fdaed341405b51d1f75d883ea7efe31c4
Christophe Taton <taton@wibidata.com>
2013-01-09 16:50:54 -0800

    SCHEMA-1. Adds a unit-test for schema validation on write.
    Also adds Kiji.getAdmin().
    
    Signed-off-by: Aaron Kimball <aaron@wibidata.com>

---
+++ kiji-schema/src/main/java/org/kiji/schema/KijiEncodingException.java
@@ -0,0 +1,55 @@
+/**
+ * (c) Copyright 2012 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.schema;
+
+import org.kiji.annotations.ApiAudience;
+
+/** Runtime exception thrown when encoding a cell's content fails. */
+@ApiAudience.Public
+public final class KijiEncodingException extends RuntimeException {
+
+  /**
+   * Initializes an encoding exception.
+   *
+   * @param message Error message.
+   */
+  public KijiEncodingException(String message) {
+    super(message);
+  }
+
+  /**
+   * Initializes an encoding exception.
+   *
+   * @param message Error message.
+   * @param throwable Underlying exception, if any.
+   */
+  public KijiEncodingException(String message, Throwable throwable) {
+    super(message, throwable);
+  }
+
+  /**
+   * Initializes an encoding exception.
+   *
+   * @param throwable Underlying exception, if any.
+   */
+  public KijiEncodingException(Throwable throwable) {
+    super(throwable);
+  }
+}
