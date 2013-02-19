8f2b09c8577e5a9a2129df6cc77751b47da550f1
Lee Sheng <lsheng@wibidata.com>
2013-02-18 20:57:45 -0800

    Update KijiURI docs

--- kiji-schema/src/main/java/org/kiji/schema/KijiURI.java
+++ kiji-schema/src/main/java/org/kiji/schema/KijiURI.java
@@ -39,8 +39,26 @@ import org.kiji.annotations.ApiAudience;
 import org.kiji.schema.util.KijiNameValidator;
 
 /**
- * URI that fully qualifies a Kiji instance/table/column.
- * Use "default" for the default Kiji instance.
+ * URI that uniquely identifies a Kiji instance, table, column(s).
+ * Use <em>kiji://.env/default/</em> for the default Kiji instance URI.
+ *
+ * <p>
+ *   KijiURI objects can be constructed directly from parsing a URI string:
+ * </p>
+ * <pre><code>
+ *   final KijiURI uri = KijiURI.newBuilder("kiji://.env/default/mytable/col").build();
+ * </code></pre>
+ *
+ * <p>
+ *   Alternately, KijiURI objects can be constructed from components by using the builder:
+ * </p>
+ * <pre><code>
+ *   final KijiURI uri = KijiURI.newBuilder()
+ *     .withInstanceName("default")
+ *     .withTableName("mytable")
+ *     .addColumnName(new KijiColumnName(col))
+ *     .build();
+ * </code></pre>
  *
  * Valid URI forms look like:
  * <li> "kiji://zkHost"
