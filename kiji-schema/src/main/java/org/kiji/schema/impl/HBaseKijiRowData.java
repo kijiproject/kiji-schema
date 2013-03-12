cf7f46f7223a917dd37e2b4395257df5a27b467d
Juliet Hougland <juliet@wibidata.com>
2013-03-12 15:31:03 -0700

    Review fixes from Lee.

--- kiji-schema/src/main/java/org/kiji/schema/impl/HBaseKijiRowData.java
+++ kiji-schema/src/main/java/org/kiji/schema/impl/HBaseKijiRowData.java
@@ -481,10 +481,10 @@ public final class HBaseKijiRowData implements KijiRowData {
   @Override
   public <T> NavigableMap<String, T> getMostRecentValues(String family) throws IOException {
     Preconditions.checkState(mTableLayout.getFamilyMap().get(family).isMapType(),
-        new IllegalStateException(String.format("getMostRecentValues(String family) is only enabled"
+        String.format("getMostRecentValues(String family) is only enabled"
         + " on map type column families. The column family [%s], is a group type column family."
         + " Please use the getMostRecentValues(String family, String qualifier) method.",
-        family)));
+        family));
     final NavigableMap<String, T> result = Maps.newTreeMap();
     for (String qualifier : getQualifiers(family)) {
       final T value = getMostRecentValue(family, qualifier);
@@ -509,10 +509,10 @@ public final class HBaseKijiRowData implements KijiRowData {
   public <T> NavigableMap<String, NavigableMap<Long, T>> getValues(String family)
       throws IOException {
     Preconditions.checkState(mTableLayout.getFamilyMap().get(family).isMapType(),
-        new IllegalStateException(String.format("getValues(String family) is only enabled on map"
+        String.format("getValues(String family) is only enabled on map"
         + "type column families. The column family [%s], is a group type column family. Please use"
         + "getValues(String family, String qualifier) method.",
-        family)));
+        family));
     final NavigableMap<String, NavigableMap<Long, T>> result = Maps.newTreeMap();
     for (String qualifier : getQualifiers(family)) {
       final NavigableMap<Long, T> timeseries = getValues(family, qualifier);
@@ -539,10 +539,10 @@ public final class HBaseKijiRowData implements KijiRowData {
   public <T> NavigableMap<String, KijiCell<T>> getMostRecentCells(String family)
       throws IOException {
     Preconditions.checkState(mTableLayout.getFamilyMap().get(family).isMapType(),
-        new IllegalStateException(String.format("getMostRecentCells(String family) is only enabled"
+        String.format("getMostRecentCells(String family) is only enabled"
         + " on map type column families. The column family [%s], is a group type column family."
         + " Please use the getMostRecentCells(String family, String qualifier) method.",
-        family)));
+        family));
     final NavigableMap<String, KijiCell<T>> result = Maps.newTreeMap();
     for (String qualifier : getQualifiers(family)) {
       final KijiCell<T> cell = getMostRecentCell(family, qualifier);
@@ -576,10 +576,10 @@ public final class HBaseKijiRowData implements KijiRowData {
   public <T> NavigableMap<String, NavigableMap<Long, KijiCell<T>>> getCells(String family)
       throws IOException {
     Preconditions.checkState(mTableLayout.getFamilyMap().get(family).isMapType(),
-        new IllegalStateException(String.format("getCells(String family) is only enabled"
+        String.format("getCells(String family) is only enabled"
         + " on map type column families. The column family [%s], is a group type column family."
         + " Please use the getCells(String family, String qualifier) method.",
-        family)));
+        family));
     final NavigableMap<String, NavigableMap<Long, KijiCell<T>>> result = Maps.newTreeMap();
     for (String qualifier : getQualifiers(family)) {
       final NavigableMap<Long, KijiCell<T>> cells = getCells(family, qualifier);
@@ -619,10 +619,10 @@ public final class HBaseKijiRowData implements KijiRowData {
   @Override
   public KijiPager getPager(String family) throws KijiColumnPagingNotEnabledException {
         Preconditions.checkState(mTableLayout.getFamilyMap().get(family).isMapType(),
-        new IllegalStateException(String.format("getPager(String family) is only enabled on map"
+        String.format("getPager(String family) is only enabled on map"
         + " type column families. The column family %s, is a group type column family. Please use"
         + " getPager(String family, String qualifier) method.",
-        family)));
+        family));
     final KijiColumnName kijiFamily = new KijiColumnName(family);
     if (kijiFamily.isFullyQualified()) {
       throw new IllegalArgumentException("Family name (" + family + ") had a colon ':' in it");
--- kiji-schema/src/test/java/org/kiji/schema/TestRowDataColumnFamilyOps.java
+++ kiji-schema/src/test/java/org/kiji/schema/TestRowDataColumnFamilyOps.java
@@ -42,13 +42,13 @@ import org.kiji.schema.util.InstanceBuilder;
 public class TestRowDataColumnFamilyOps extends KijiClientTest {
   private static final Logger LOG = LoggerFactory.getLogger(TestRowDataColumnFamilyOps.class);
   private Kiji mKiji;
+  private KijiRowData mRow1;
 
   @Before
   public void setupInstance() throws Exception {
    final KijiTableLayout layout =
       KijiTableLayout.newLayout(KijiTableLayouts.getLayout(
           KijiTableLayouts.TWO_COLUMN_DIFFERENT_TYPES));
-    //
    mKiji = new InstanceBuilder()
         .withTable("table", layout)
             .withRow("row1")
@@ -57,6 +57,11 @@ public class TestRowDataColumnFamilyOps extends KijiClientTest {
                     .withValue(1L, "I am a String")
                   .withQualifier("column2").withValue(1L, 1)
         .build();
+     final KijiTable table = mKiji.openTable("table");
+     final KijiTableReader reader = table.openTableReader();
+     final KijiRowData row1 = reader.get(table.getEntityId("row1"),
+              KijiDataRequest.create("family"));
+
   }
 
   @After
@@ -66,56 +71,57 @@ public class TestRowDataColumnFamilyOps extends KijiClientTest {
 
   @Test(expected=IllegalStateException.class)
   public void testGetValues() throws IOException {
-     final KijiTable table = mKiji.openTable("table");
-     final KijiTableReader reader = table.openTableReader();
-     final KijiRowData row1 = reader.get(table.getEntityId("row1"),
-              KijiDataRequest.create("family"));
-     assertTrue("Row does not contain column family [family].", row1.containsColumn("family"));
+         assertTrue("Row does not contain column family [family].", mRow1.containsColumn("family"));
      assertTrue("Row does not contain column [family:column1].",
-         row1.containsColumn("family", "column1"));
+         mRow1.containsColumn("family", "column1"));
      assertTrue("Row does not contain column [family:column2].",
-         row1.containsColumn("family", "column2"));
-     final NavigableMap<String, NavigableMap<Long, CharSequence>> stringGet = row1.getValues(
+         mRow1.containsColumn("family", "column2"));
+     final NavigableMap<String, NavigableMap<Long, CharSequence>> stringGet = mRow1.getValues(
          "family");
-     final NavigableMap<String, NavigableMap<Long, Integer>> intGet = row1.getValues("family");
+     final NavigableMap<String, NavigableMap<Long, Integer>> intGet = mRow1.getValues("family");
      assertEquals("I am not a string! () for the stringGet.", "I am a String",
          stringGet.get("column1").get(1L).toString());
-     intGet.get("column1").get(1L);
+     try {
+       intGet.get("column1").get(1L);
+     } catch (IllegalStateException ex) {
+       assertEquals("Incorrect Exception message: ", "getValues(String family) is only enabled on map"
+        + "type column families. The column family [family], is a group type column family. Please use"
+        + "getValues(String family, String qualifier) method.", ex.getMessage());
+     }
   }
 
   @Test(expected=IllegalStateException.class)
   public void testGetMostRecentValues() throws IOException {
-     final KijiTable table = mKiji.openTable("table");
-     final KijiTableReader reader = table.openTableReader();
-     final KijiRowData row1 = reader.get(table.getEntityId("row1"),
-              KijiDataRequest.create("family"));
-     assertTrue("Row does not contain column family [family].", row1.containsColumn("family"));
+     assertTrue("Row does not contain column family [family].", mRow1.containsColumn("family"));
      assertTrue("Row does not contain column [family:column1].",
-         row1.containsColumn("family", "column1"));
+         mRow1.containsColumn("family", "column1"));
      assertTrue("Row does not contain column [family:column2].",
-         row1.containsColumn("family", "column2"));
+         mRow1.containsColumn("family", "column2"));
      final NavigableMap<String, CharSequence> stringGet =
-         row1.getMostRecentValues("family");
-     final NavigableMap<String, Integer> intGet = row1.getMostRecentValues("family");
+         mRow1.getMostRecentValues("family");
+     final NavigableMap<String, Integer> intGet = mRow1.getMostRecentValues("family");
      assertEquals("I am not a string! () for the stringGet.", "I am a String",
          stringGet.get("column1").toString());
-     intGet.get("column1");
+     try {
+       intGet.get("column1");
+     } catch (IllegalStateException ex) {
+       assertEquals("Incorrect Exception message: ", "getMostRecentValues(String family) is only "
+         + "enabled on map type column families. The column family [family], is a group type column"
+         + "family. Please use getMostRecentValues(String family, String qualifier) method.",
+         ex.getMessage());
+     }
   }
 
   @Test(expected=IllegalStateException.class)
   public void testGetMostRecentCells() throws IOException {
-     final KijiTable table = mKiji.openTable("table");
-     final KijiTableReader reader = table.openTableReader();
-     final KijiRowData row1 = reader.get(table.getEntityId("row1"),
-              KijiDataRequest.create("family"));
-     assertTrue("Row does not contain column family [family].", row1.containsColumn("family"));
+     assertTrue("Row does not contain column family [family].", mRow1.containsColumn("family"));
      assertTrue("Row does not contain column [family:column1].",
-         row1.containsColumn("family", "column1"));
+         mRow1.containsColumn("family", "column1"));
      assertTrue("Row does not contain column [family:column2].",
-         row1.containsColumn("family", "column2"));
+         mRow1.containsColumn("family", "column2"));
      final NavigableMap<String, KijiCell<CharSequence>> stringGet =
-         row1.getMostRecentCells("family");
-     final NavigableMap<String, KijiCell<Integer>> intGet = row1.getMostRecentCells("family");
+         mRow1.getMostRecentCells("family");
+     final NavigableMap<String, KijiCell<Integer>> intGet = mRow1.getMostRecentCells("family");
      assertEquals("I am not a string! () for the stringGet.", "I am a String",
          stringGet.get("column1").getData().toString());
      assertEquals("I am not a string! () for the intGet.", "I am a String", intGet.get("column1")
@@ -124,19 +130,15 @@ public class TestRowDataColumnFamilyOps extends KijiClientTest {
 
   @Test(expected=IllegalStateException.class)
   public void testGetCells() throws IOException {
-     final KijiTable table = mKiji.openTable("table");
-     final KijiTableReader reader = table.openTableReader();
-     final KijiRowData row1 = reader.get(table.getEntityId("row1"),
-              KijiDataRequest.create("family"));
-     assertTrue("Row does not contain column family [family].", row1.containsColumn("family"));
+     assertTrue("Row does not contain column family [family].", mRow1.containsColumn("family"));
      assertTrue("Row does not contain column [family:column1].",
-         row1.containsColumn("family", "column1"));
+         mRow1.containsColumn("family", "column1"));
      assertTrue("Row does not contain column [family:column2].",
-         row1.containsColumn("family", "column2"));
+         mRow1.containsColumn("family", "column2"));
      final NavigableMap<String, NavigableMap<Long, KijiCell<CharSequence>>> stringGet =
-         row1.getCells("family");
+         mRow1.getCells("family");
      final NavigableMap<String, NavigableMap<Long, KijiCell<Integer>>> intGet =
-         row1.getCells("family");
+         mRow1.getCells("family");
      assertEquals("I am not a string! () for the stringGet.", "I am a String",
          stringGet.get("column1").get(1L).getData().toString());
      assertEquals("I am not a string! () for the intGet.", "I am a String", intGet.get("column1")
@@ -145,10 +147,10 @@ public class TestRowDataColumnFamilyOps extends KijiClientTest {
 
   @Test(expected=IllegalStateException.class)
   public void testGetPager() throws IOException {
-     final KijiTable table = mKiji.openTable("table");
-     final KijiTableReader reader = table.openTableReader();
-     KijiDataRequestBuilder builder = KijiDataRequest.builder();
-     builder.newColumnsDef().withMaxVersions(5).withPageSize(2).addFamily("family");
+    final KijiTable table = mKiji.openTable("table");
+    final KijiTableReader reader = table.openTableReader();
+    KijiDataRequestBuilder builder = KijiDataRequest.builder();
+    builder.newColumnsDef().withMaxVersions(5).withPageSize(2).addFamily("family");
     final KijiDataRequest dataRequest = builder.build();
     final KijiRowData row1 = reader.get(table.getEntityId("row1"),
               dataRequest);
