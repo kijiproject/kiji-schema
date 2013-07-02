eb3e407d217613a804b51f6b15af341db7d702fb
Christophe Taton <taton@wibidata.com>
2013-07-02 15:21:45 -0700

    SCHEMA-455. Expose the len-as-val feature from KeyOnlyFilter in StripValueColumnFilter.
    Adds a CellByteSizeFilter that exposes the len-as-val feature.
    
    issue: https://jira.kiji.org/browse/SCHEMA-455
    review: https://review.kiji.org/r/760

---
+++ kiji-schema/src/main/java/org/kiji/schema/filter/CellByteSizeAsValueFilter.java
@@ -0,0 +1,108 @@
+/**
+ * (c) Copyright 2013 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.schema.filter;
+
+import java.io.IOException;
+
+import com.google.common.base.Objects;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.KeyOnlyFilter;
+
+import org.kiji.annotations.ApiAudience;
+import org.kiji.annotations.ApiStability;
+import org.kiji.schema.KijiColumnName;
+
+/**
+ * Filter that replaces the value of each cell by its size, in number of bytes.
+ *
+ * <p> Each cell size is encoded as a 32 bits integer (see {@link Bytes#toInt(byte[])}). </p>
+ * <p>
+ *   In order to use this filter, you must override the reader schema of the affected columns
+ *   as a final fixed byte array of size 4.
+ *
+ *   <pre><tt>{@code
+ *   final KijiTable table = ...;
+ *   final KijiTableLayout layout = table.getLayout();
+ *   final KijiColumnName column = new KijiColumnName("family", "qualifier");
+ *   final Map<KijiColumnName, CellSpec> overrides =
+ *       ImmutableMap.<KijiColumnName, CellSpec>builder()
+ *       .put(column, layout.getCellSpec(column)
+ *           .setCellSchema(CellSchema.newBuilder()
+ *               .setType(SchemaType.INLINE)
+ *               .setStorage(SchemaStorage.FINAL)
+ *               .setValue("{\"type\": \"fixed\", \"size\": 4, \"name\": \"Int32\"}")
+ *               .build()))
+ *       .build();
+ *
+ *   final KijiTableReader reader = table.getReaderFactory().openTableReader(overrides);
+ *   try {
+ *     final KijiDataRequest dataRequest = KijiDataRequest.builder()
+ *         .addColumns(ColumnsDef.create().withFilter(new CellByteSizeAsValueFilter()).add(column))
+ *         .build();
+ *     final EntityId eid = table.getEntityId(...);
+ *     final KijiRowData row = reader.get(eid, dataRequest);
+ *     final GenericData.Fixed fixed32 = row.getMostRecentValue("family", "qualifier");
+ *     final int cellSize = Bytes.toInt(fixed32.bytes());
+ *     ...
+ *   } finally {
+ *     reader.close();
+ *   }
+ *   }</tt></pre>
+ * </p>
+ * <p> TODO(SCHEMA-334): This filter may currently not be combined properly via {@link FilterList}.
+ *     If you need to apply this filter to specific columns, you must currently send separate get
+ *     requests.
+ * </p>
+ */
+@ApiAudience.Framework
+@ApiStability.Experimental
+public final class CellByteSizeAsValueFilter extends KijiColumnFilter {
+  private static final long serialVersionUID = 1L;
+
+  /** All StripValueColumnFilter instances are the same: generate hash-code ahead of time. */
+  private static final int HASH_CODE =
+      new HashCodeBuilder().append(CellByteSizeAsValueFilter.class).toHashCode();
+
+  /** {@inheritDoc} */
+  @Override
+  public Filter toHBaseFilter(KijiColumnName kijiColumnName, Context context) throws IOException {
+    return new KeyOnlyFilter(true);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public boolean equals(Object other) {
+    // All StripValueRowFilters are the same.
+    return other instanceof CellByteSizeAsValueFilter;
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public int hashCode() {
+    return HASH_CODE;
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public String toString() {
+    return Objects.toStringHelper(CellByteSizeAsValueFilter.class).toString();
+  }
+}
---
+++ kiji-schema/src/test/java/org/kiji/schema/filter/TestCellByteSizeAsValueFilter.java
@@ -0,0 +1,96 @@
+/**
+ * (c) Copyright 2012 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.schema.filter;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.avro.generic.GenericData;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.Test;
+
+import org.kiji.schema.EntityId;
+import org.kiji.schema.Kiji;
+import org.kiji.schema.KijiClientTest;
+import org.kiji.schema.KijiColumnName;
+import org.kiji.schema.KijiDataRequest;
+import org.kiji.schema.KijiDataRequestBuilder.ColumnsDef;
+import org.kiji.schema.KijiRowData;
+import org.kiji.schema.KijiTable;
+import org.kiji.schema.KijiTableReader;
+import org.kiji.schema.avro.CellSchema;
+import org.kiji.schema.avro.SchemaStorage;
+import org.kiji.schema.avro.SchemaType;
+import org.kiji.schema.layout.CellSpec;
+import org.kiji.schema.layout.KijiTableLayout;
+import org.kiji.schema.layout.KijiTableLayouts;
+import org.kiji.schema.util.InstanceBuilder;
+
+/** Tests the StripValueRowFilter. */
+public class TestCellByteSizeAsValueFilter extends KijiClientTest {
+  /** Tests that the CellByteSizeAsValueFilter replaces the value by its size, in bytes. */
+  @Test
+  public void testCellByteSizeAsValueFilter() throws Exception {
+    final Kiji kiji = new InstanceBuilder(getKiji())
+        .withTable(KijiTableLayouts.getLayout(KijiTableLayouts.SIMPLE))
+            .withRow("row")
+                .withFamily("family")
+                    .withQualifier("column")
+                        .withValue("0123456789")
+        .build();
+
+    final KijiTable table = kiji.openTable("table");
+    try {
+      final EntityId eid = table.getEntityId("row");
+
+      final KijiTableLayout layout = table.getLayout();
+      final KijiColumnName column = new KijiColumnName("family", "column");
+      final Map<KijiColumnName, CellSpec> overrides =
+          ImmutableMap.<KijiColumnName, CellSpec>builder()
+          .put(column, layout.getCellSpec(column)
+              .setCellSchema(CellSchema.newBuilder()
+                  .setType(SchemaType.INLINE)
+                  .setStorage(SchemaStorage.FINAL)
+                  .setValue("{\"type\": \"fixed\", \"size\": 4, \"name\": \"Int32\"}")
+                  .build()))
+          .build();
+
+      final KijiTableReader reader = table.getReaderFactory().openTableReader(overrides);
+      try {
+        final KijiDataRequest dataRequest = KijiDataRequest.builder()
+            .addColumns(ColumnsDef.create().withFilter(new CellByteSizeAsValueFilter()).add(column))
+            .build();
+        final KijiRowData row = reader.get(eid, dataRequest);
+        final GenericData.Fixed fixed32 = row.getMostRecentValue("family", "column");
+        final int cellSize = Bytes.toInt(fixed32.bytes());
+
+        // Cell size is: length(MD5-hash) + len(string size) + len(string)
+        assertEquals(16 + 1 + 10, cellSize);
+
+      } finally {
+        reader.close();
+      }
+    } finally {
+      table.release();
+    }
+  }
+}
