3818c4e3cd7f01985fadf3f66819761da328a124
Christophe Taton <taton@wibidata.com>
2014-06-03 16:59:26 -0700

    SCHEMA-806. Revamp (fix and improve) kiji shell script.
     - Improve DEBUG mode (logs file:line)
     - Document environment variables.
     - Emit warnings when required environment variables are not set.
     - Add kiji env-check to dump and validate the environment.
     - Document priorities between environment and path.
     - Disable Maven assembly filtering of shell-scripts.
     - Beef-up profiling enable/disable scripts.
    
    issue: https://jira.kiji.org/browse/SCHEMA-806
    review: https://review.kiji.org/r/1903

--- kiji-schema/src/main/scripts/disable-profiling.sh
+++ kiji-schema/src/main/scripts/disable-profiling.sh
@@ -1,48 +1,68 @@
 #!/usr/bin/env bash
-#
-#   (c) Copyright 2013 WibiData, Inc.
-#
-#   See the NOTICE file distributed with this work for additional
-#   information regarding copyright ownership.
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
-#   The disable-profiling script performs clean up so that you can use the
-#   standard (non-profiling) jars after you are done profiling.
-#
-#   The sequence of commands is as follows:
-#
-#   $KIJI_HOME/bin/profiling/enable-profiling.sh
-#   kiji <command> etc...
-#   $KIJI_HOME/bin/profiling/disable-profiling.sh
 
-set -e
+# (c) Copyright 2012 WibiData, Inc.
+#
+# See the NOTICE file distributed with this work for additional
+# information regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-if [ -z "${KIJI_HOME}" ]; then
+# ------------------------------------------------------------------------------
+
+# The disable-profiling script performs clean up so that you can use the
+# standard (non-profiling) jars after you are done profiling.
+#
+# The sequence of commands is as follows:
+#
+# $KIJI_HOME/bin/profiling/enable-profiling.sh
+# kiji <command> etc...
+# $KIJI_HOME/bin/profiling/disable-profiling.sh
+
+# ------------------------------------------------------------------------------
+
+set -o nounset   # Fail when referencing undefined variables
+set -o errexit   # Script exits on the first error
+set -o pipefail  # Pipeline status failure if any command fails
+if [[ ! -z "${DEBUG:-}" ]]; then
+  source=$(basename "${BASH_SOURCE}")
+  PS4="# ${source}":'${LINENO}: '
+  set -x
+fi
+
+# ------------------------------------------------------------------------------
+
+if [[ -z "${KIJI_HOME}" ]]; then
   echo "Please set the KIJI_HOME enviroment variable before you enable profiling."
   exit 1
 fi
 
-if [ -z "${HADOOP_HOME}" ]; then
+if [[ ! -f "${KIJI_HOME}/conf/kiji-schema.version" ]]; then
+  error "Invalid KIJI_HOME=${KIJI_HOME}"
+  error "Cannot find \${KIJI_HOME}/conf/kiji-schema.version"
+  exit 1
+fi
+kiji_schema_version=$(cat "${KIJI_HOME}/conf/kiji-schema.version")
+
+if [[ -z "${HADOOP_HOME}" ]]; then
   echo "Please set the HADOOP_HOME environment variable before you enable profiling."
   exit 1
 fi
 
 # Name of the profiling version of the kiji schema jar
-kiji_profiling_schema_jar_name="kiji-schema-${project.version}-profiling.jar"
+kiji_profiling_schema_jar_name="kiji-schema-${kiji_schema_version}-profiling.jar"
 
 # Name of the original kiji schema jar
-kiji_schema_jar_name="kiji-schema-${project.version}.jar"
+kiji_schema_jar_name="kiji-schema-${kiji_schema_version}.jar"
 
 # We may have Hadoop distribution-specific jars to load in
 # $KIJI_HOME/lib/distribution/hadoopN, where N is the major digit of the Hadoop
@@ -50,8 +70,8 @@ kiji_schema_jar_name="kiji-schema-${project.version}.jar"
 
 # Detect and extract the current Hadoop version number. e.g. "Hadoop 2.x-..." -> "2"
 # You can override this with $KIJI_HADOOP_DISTRO_VER (e.g. "hadoop1" or "hadoop2").
-hadoop_major_version=`$HADOOP_HOME/bin/hadoop version | head -1 | cut -c 8`
-if [ -z "$hadoop_major_version" -a -z "$KIJI_HADOOP_DISTRO_VER" ]; then
+hadoop_major_version=$($HADOOP_HOME/bin/hadoop version | head -1 | cut -c 8)
+if [[ -z "$hadoop_major_version" && -z "$KIJI_HADOOP_DISTRO_VER" ]]; then
   echo "Warning: Unknown Hadoop version. May not be able to load all Kiji jars."
   echo "Set KIJI_HADOOP_DISTRO_VER to 'hadoop1' or 'hadoop2' to load these."
 else
@@ -67,8 +87,8 @@ kiji_mr_jar_prefix="kiji-mapreduce-${KIJI_HADOOP_DISTRO_VER}-"
 # so that they may be restored later.
 orig_dir="${KIJI_HOME}/lib/original_jars"
 
-# Flag to indicate if something unexpected was found and disabling profiling requires
-# any manual intervention.
+# Flag to indicate if something unexpected was found and disabling profiling
+# requires any manual intervention.
 inconsistent_state="false"
 
 # Name of aspectj jar
@@ -76,7 +96,7 @@ aspectj_jar_name="aspectjrt-1.7.2.jar"
 
 # Remove the profiling jars from lib and distrodir. We have cp'd them while
 # enabling profiling, so rm should be fine.
-if [ -f "${KIJI_HOME}/lib/${kiji_profiling_schema_jar_name}" ]; then
+if [[ -f "${KIJI_HOME}/lib/${kiji_profiling_schema_jar_name}" ]]; then
   echo "Removing profile enabled kiji schema jar..."
   rm -f "${KIJI_HOME}/lib/${kiji_profiling_schema_jar_name}"
 else
@@ -86,7 +106,7 @@ else
 fi
 
 # Remove the aspectj jar
-if [ -f "${KIJI_HOME}/lib/${aspectj_jar_name}" ]; then
+if [[ -f "${KIJI_HOME}/lib/${aspectj_jar_name}" ]]; then
   echo "Removing aspectj jar..."
   rm -f "${KIJI_HOME}/lib/${aspectj_jar_name}"
 else
@@ -96,10 +116,11 @@ else
 fi
 
 # Remove the KijiMR profiling-enabled jar
-if [ -d "${distrodir}" ]; then
-  if [ -f "${distrodir}/${kiji_mr_jar_prefix}"*"profiling.jar" ]; then
+if [[ -d "${distrodir}" ]]; then
+  profiling_jar="${distrodir}/${kiji_mr_jar_prefix}"*"profiling.jar"
+  if [[ -f "${profiling_jar}" ]]; then
     echo "Remove profile enabled kiji mapreduce jar..."
-    rm -f "${distrodir}/${kiji_mr_jar_prefix}"*"profiling.jar"
+    rm -f "${profiling_jar}"
   else
     echo "Did not find ${kiji_mr_jar_prefix}*-profiling.jar in ${distrodir}. "
     echo "Is profiling enabled?"
@@ -109,19 +130,21 @@ fi
 
 # Check if the orig_dir exists and move the schema and mapreduce jars into their
 # rightful places
-if [ -d "${orig_dir}" ]; then
-  if [ ! -f "${orig_dir}/${kiji_schema_jar_name}" ]; then
-    echo "Cannot find original schema jar in ${orig_dir}. Please move the jar" \
-    " ${kiji_schema_jar_name} to ${KIJI_HOME}/lib"
+if [[ -d "${orig_dir}" ]]; then
+  if [[ ! -f "${orig_dir}/${kiji_schema_jar_name}" ]]; then
+    echo "Cannot find original schema jar in ${orig_dir}. " \
+      "Please move the jar ${kiji_schema_jar_name} to ${KIJI_HOME}/lib"
     inconsistent_state="true"
   else
     echo "Moving ${orig_dir}/${kiji_schema_jar_name} to ${KIJI_HOME}/lib/ ..."
     mv "${orig_dir}/${kiji_schema_jar_name}" "${KIJI_HOME}/lib/"
   fi
 
-  if [ ! -f "${orig_dir}/${kiji_mr_jar_prefix}"*".jar" ]; then
-    echo "Cannot find the original KijiMR jar in ${orig_dir}. Please move the" \
-    "jar named ${kiji_mr_jar_prefix}*.jar to the ${distrodir} directory."
+  kijimr_jar="${orig_dir}/${kiji_mr_jar_prefix}"*".jar"
+  if [[ ! -f "${kijimr_jar}" ]]; then
+    echo "Cannot find the original KijiMR jar in ${orig_dir}. " \
+      "Please move the jar named ${kiji_mr_jar_prefix}*.jar " \
+      "to the ${distrodir} directory."
     inconsistent_state="true"
   else
     echo "Moving ${orig_dir}/${kiji_mr_jar_prefix}-* to ${distrodir}"
@@ -130,24 +153,25 @@ if [ -d "${orig_dir}" ]; then
 else
   echo "Did not find ${orig_dir}. This may be because profiling was not enabled."
   echo "Ensure that ${KIJI_HOME}/lib/ and ${distrodir} have the files " \
-  "${kiji_schema_jar_name} and ${kiji_mr_jar_prefix}-*.jar respectively."
+    "${kiji_schema_jar_name} and ${kiji_mr_jar_prefix}-*.jar respectively."
   inconsistent_state="true"
 fi
 
 # Remove the directory which was holding the normal jars
-if [ -d "${orig_dir}" ]; then
+if [[ -d "${orig_dir}" ]]; then
   rmdir "${orig_dir}"
 else
   echo "Directory ${orig_dir} not found."
   inconsistent_state="true"
 fi
 
-if [ "${inconsistent_state}" == "false" ]; then
+if [[ "${inconsistent_state}" == "false" ]]; then
   echo ""
-  echo "Profiling jars have been disabled. The normal Kiji modules have been restored."
+  echo "Profiling jars have been disabled. " \
+    "The normal Kiji modules have been restored."
   echo ""
 else
-  echo "Please check the error messages. Some manual actions may be required to disable" \
-  "profiling."
+  echo "Please check the error messages. " \
+    "Some manual actions may be required to disable profiling."
   exit 1
 fi
--- kiji-schema/src/main/scripts/enable-profiling.sh
+++ kiji-schema/src/main/scripts/enable-profiling.sh
@@ -1,49 +1,69 @@
 #!/usr/bin/env bash
-#
-#   (c) Copyright 2013 WibiData, Inc.
-#
-#   See the NOTICE file distributed with this work for additional
-#   information regarding copyright ownership.
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
-#   The enable-profiling script performs the actions required to use the
-#   profiling jars for KijiSchema and KijiMR, instead of standard jars.
-#   The corresponding disable-profiling script performs clean up so that
-#   you can use the standard (non-profiling) jars again.
-#
-#   Run the command as follows before running any kiji commands
-#
-#   $KIJI_HOME/bin/profiling/enable-profiling.sh
-#   kiji <command> etc...
-#   $KIJI_HOME/bin/profiling/disable-profiling.sh
 
-set -e
+# (c) Copyright 2012 WibiData, Inc.
+#
+# See the NOTICE file distributed with this work for additional
+# information regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-if [ -z "${KIJI_HOME}" ]; then
+# ------------------------------------------------------------------------------
+
+# The enable-profiling script performs the actions required to use the
+# profiling jars for KijiSchema and KijiMR, instead of standard jars.
+# The corresponding disable-profiling script performs clean up so that
+# you can use the standard (non-profiling) jars again.
+#
+# Run the command as follows before running any kiji commands
+#
+# $KIJI_HOME/bin/profiling/enable-profiling.sh
+# kiji <command> etc...
+# $KIJI_HOME/bin/profiling/disable-profiling.sh
+
+# ------------------------------------------------------------------------------
+
+set -o nounset   # Fail when referencing undefined variables
+set -o errexit   # Script exits on the first error
+set -o pipefail  # Pipeline status failure if any command fails
+if [[ ! -z "${DEBUG:-}" ]]; then
+  source=$(basename "${BASH_SOURCE}")
+  PS4="# ${source}":'${LINENO}: '
+  set -x
+fi
+
+# ------------------------------------------------------------------------------
+
+if [[ -z "${KIJI_HOME}" ]]; then
   echo "Please set the KIJI_HOME enviroment variable before you enable profiling."
   exit 1
 fi
 
-if [ -z "${HADOOP_HOME}" ]; then
+if [[ ! -f "${KIJI_HOME}/conf/kiji-schema.version" ]]; then
+  error "Invalid KIJI_HOME=${KIJI_HOME}"
+  error "Cannot find \${KIJI_HOME}/conf/kiji-schema.version"
+  exit 1
+fi
+kiji_schema_version=$(cat "${KIJI_HOME}/conf/kiji-schema.version")
+
+if [[ -z "${HADOOP_HOME}" ]]; then
   echo "Please set the HADOOP_HOME environment variable before you enable profiling."
   exit 1
 fi
 
 # Name of the profiled version of the KijiSchema jar
-profiling_kiji_schema_jar_name="kiji-schema-${project.version}-profiling.jar"
+profiling_kiji_schema_jar_name="kiji-schema-${kiji_schema_version}-profiling.jar"
 # Name of the original KijiSchema jar
-kiji_schema_jar_name="kiji-schema-${project.version}.jar"
+kiji_schema_jar_name="kiji-schema-${kiji_schema_version}.jar"
 
 # We may have Hadoop distribution-specific jars to load in
 # $KIJI_HOME/lib/distribution/hadoopN, where N is the major digit of the Hadoop
@@ -51,12 +71,12 @@ kiji_schema_jar_name="kiji-schema-${project.version}.jar"
 
 # Detect and extract the current Hadoop version number. e.g. "Hadoop 2.x-..." -> "2"
 # You can override this with $KIJI_HADOOP_DISTRO_VER (e.g. "hadoop1" or "hadoop2").
-hadoop_major_version=`$HADOOP_HOME/bin/hadoop version | head -1 | cut -c 8`
-if [ -z "$hadoop_major_version" -a -z "$KIJI_HADOOP_DISTRO_VER" ]; then
+hadoop_major_version=$("${HADOOP_HOME}/bin/hadoop" version | head -1 | cut -c 8)
+if [[ -z "$hadoop_major_version" && -z "$KIJI_HADOOP_DISTRO_VER" ]]; then
   echo "Warning: Unknown Hadoop version. May not be able to load all Kiji jars."
   echo "Set KIJI_HADOOP_DISTRO_VER to 'hadoop1' or 'hadoop2' to load these."
 else
-  KIJI_HADOOP_DISTRO_VER=${KIJI_HADOOP_DISTRO_VER:-"hadoop$hadoop_major_version"}
+  KIJI_HADOOP_DISTRO_VER=${KIJI_HADOOP_DISTRO_VER:-"hadoop${hadoop_major_version}"}
 fi
 
 distrodir="$KIJI_HOME/lib/distribution/$KIJI_HADOOP_DISTRO_VER"
@@ -79,7 +99,7 @@ mkdir -p "${orig_dir}"
 inconsistent_state="false"
 
 # Move KijiSchema jar out of the way
-if [ -f "${KIJI_HOME}/lib/${kiji_schema_jar_name}" ]; then
+if [[ -f "${KIJI_HOME}/lib/${kiji_schema_jar_name}" ]]; then
   echo "Moving the non-profiled KijiSchema jar from "\
   "${KIJI_HOME}/lib/${kiji_schema_jar_name} to ${orig_dir}"
   mv "${KIJI_HOME}/lib/${kiji_schema_jar_name}" "${orig_dir}"
@@ -90,7 +110,7 @@ else
 fi
 
 # Copy profiling KijiSchema jar into the $KIJI_HOME/lib directory
-if [ ! -f "${KIJI_HOME}/lib/${profiling_kiji_schema_jar_name}" ]; then
+if [[ ! -f "${KIJI_HOME}/lib/${profiling_kiji_schema_jar_name}" ]]; then
   echo "Moving the profiling enabled KijiSchema jar from " \
   "${KIJI_HOME}/lib/profiling/${profiling_kiji_schema_jar_name} to ${KIJI_HOME}/lib"
   cp "${KIJI_HOME}/lib/profiling/${profiling_kiji_schema_jar_name}" "${KIJI_HOME}/lib"
@@ -100,7 +120,7 @@ else
 fi
 
 # Copy the aspectj jar into the $KIJI_HOME/lib directory
-if [ ! -f "${KIJI_HOME}/lib/${aspectj_jar_name}" ]; then
+if [[ ! -f "${KIJI_HOME}/lib/${aspectj_jar_name}" ]]; then
   echo "Moving the aspectj jar from " \
   "${KIJI_HOME}/lib/profiling/${aspectj_jar_name} to ${KIJI_HOME}/lib"
   cp "${KIJI_HOME}/lib/profiling/${aspectj_jar_name}" "${KIJI_HOME}/lib"
@@ -109,15 +129,16 @@ else
   inconsistent_state="true"
 fi
 
-# Check if distrodir contains a mapreduce jar that does not have profiling in its name
-# Move the other kijiMR jar out of the way
+# Check if distrodir contains a mapreduce jar that does not have profiling in
+# its name.  Move the other kijiMR jar out of the way
 
 # Check if distrodir exists, i.e. we are in a BentoBox
-if [ -d "${distrodir}" ]; then
+if [[ -d "${distrodir}" ]]; then
   has_kijimr_profile_jar="false"
   for fname in "${distrodir}/${kiji_mr_jar_prefix}"*.jar; do
-    # We need double brackets for the if condition to ensure the condition is satisfied
-    # when profiling is a substring of fname. This fails with single brackets.
+    # We need double brackets for the if condition to ensure the condition is
+    # satisfied when profiling is a substring of fname.
+    # This fails with single brackets.
     if [[ "${fname}" == *"profiling.jar" ]]; then
       # We found a profiling jar already installed. Set a flag so we don't
       # clobber it with another profiling jar
@@ -128,20 +149,21 @@ if [ -d "${distrodir}" ]; then
     fi
   done
 
-  if [ "${has_kijimr_profile_jar}" != "true" ]; then
+  if [[ "${has_kijimr_profile_jar}" != "true" ]]; then
     cp "${KIJI_HOME}/lib/profiling/${kiji_mr_jar_prefix}"* "${distrodir}"
   else
-    echo "A KijiMR profiling jar is already installed at ${distrodir}. Not overwriting."
+    echo "A KijiMR profiling jar is already installed at ${distrodir}. " \
+      "Not overwriting."
     inconsistent_state="true"
   fi
 fi
 
-if [ "${inconsistent_state}" == "false" ]; then
+if [[ "${inconsistent_state}" == "false" ]]; then
   echo ""
   echo "Profiling has been enabled."
   echo ""
 else
-  echo "Please check the error messages. Some manual actions may be required to enable" \
-  "profiling."
+  echo "Please check the error messages. " \
+    "Some manual actions may be required to enable profiling."
   exit 1
 fi
